3ebef6d5fc01da1d024a7d81e85a9506b5d6079e
6xingyu <118302684+6xingyu@users.noreply.github.com>
2022-12-04 14:19:49 +0800

    Update Mini-修然脚本.lua

--- 脚本/Mini-修然脚本.lua
+++ 脚本/Mini-修然脚本.lua
@@ -1,3 +1,24 @@
 
-gg.alert("全网启动次数")
+Sukor=gg.makeRequest("http://yyzn27.cn/lin.php").content --启动次数网址
+
+XR=Sukor:match("全网启动次数\":(.-),\"")  --这个不要改，改了会错误
+
+function pass()
+gg.alert("密码正确")
+end
+function nopass()
+gg.alert("密码错误")
+input = gg.prompt({"全网启动次数:"..XR.."次\n每日随机密码\n加QQ群873231717 在群内@Q群管家查看密码"}, {[1] = ''}, {[1] = "text"}) 
+if input[1]=="982556" then
+pass()
+else
+nopass()
+end
+end
+input = gg.prompt({"全网启动次数:"..XR.."次\n每日随机密码\n加QQ群873231717"}, {[1] = ''}, {[1] = "text"}) 
+if input[1]=="982556" then
+pass()
+else
+nopass()
+end
 
