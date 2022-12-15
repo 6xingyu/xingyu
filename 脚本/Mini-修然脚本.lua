71337e1cd2f5a9cd057a963b92b5063825122013
6xingyu <118302684+6xingyu@users.noreply.github.com>
2022-12-15 13:03:12 +0800

    Update Mini-修然脚本.lua

--- 脚本/Mini-修然脚本.lua
+++ 脚本/Mini-修然脚本.lua
@@ -1,1 +1,25 @@
-gg.alert("访问服务器失败！\n请检查网络后重试,或者是你手机问题")
+function pass()
+
+gg.alert("密码正确")
+
+end
+
+function nopass()
+
+gg.alert("密码错误")
+
+os.exit()
+
+end
+
+input = gg.prompt({"请输入密码"}, {[1] = 0}, {[1] = "text"}) 
+
+if input[1]=="666" then ---这里的666就是密码，666可以改成任意你想改的密码
+
+pass()
+
+else
+
+nopass()
+
+end
